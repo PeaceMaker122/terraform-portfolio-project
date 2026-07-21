16d0d45b7df540c517590fd4a66d38f76b93eb40
Stiaan <214525680+PeaceMaker122@users.noreply.github.com>
2026-07-21 23:20:50 +0200

    Merge nextjs-blog into main repo, remove nested git submodule

---
+++ nextjs-blog/terraform-nextjs/main.tf
@@ -0,0 +1,133 @@
+provider "aws" {
+  region = "af-south-1"
+  
+}
+
+
+
+# Configure the AWS Provider
+# Write Terraform resource code to create desired AWS resources.                                                
+# Initialize the AWS provider to interact with AWS services.                                                        (Terraform Init)
+# Plan Terraform configurations to ensure they are syntactically correct and to see what actions will be taken.     (Terraform Plan)
+# Apply the Terraform configurations to create, update, or delete resources in your AWS environment.                (Terraform Apply)
+# Destroy the Terraform-managed infrastructure when it is no longer needed.                                         (Terraform Destroy)
+
+
+
+
+# S3 Bucket for Hosting Next.js Portfolio Website
+
+resource "aws_s3_bucket" "website" {
+  bucket = "nextjs-portfolio-website-bucket"
+  
+  tags = {
+    Name = "Portfolio Website"
+    Environment = "Production"
+  }
+}
+
+# Ownership Controls to enforce bucket owner full control
+
+resource "aws_s3_bucket_ownership_controls" "website_ownership_controls" {
+  bucket = aws_s3_bucket.website.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+# Public Access Block Configuration to allow public access for website hosting
+
+resource "aws_s3_bucket_public_access_block" "website_public_access_block" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+
+# S3 Bucket ACL to make the bucket objects publicly readable
+
+resource "aws_s3_bucket_acl" "website_acl" {
+
+  depends_on = [aws_s3_bucket_public_access_block.website_public_access_block, aws_s3_bucket_ownership_controls.website_ownership_controls]
+  bucket    = aws_s3_bucket.website.id
+  acl       = "public-read"
+}
+
+# S3 Bucket Policy for Next.js Portfolio Website
+
+resource "aws_s3_bucket_policy" "website_policy" {
+  bucket = aws_s3_bucket.website.id
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "PublicReadGetObject"
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.website.arn}/*"
+      }
+    ]
+  })
+}
+
+# Origin Access Identity for CloudFront to access S3 bucket
+
+resource "aws_cloudfront_origin_access_identity" "website_oai" {
+  comment = "OAI for Next.js Portfolio Website"
+}
+
+# CloudFront Distribution for Next.js Portfolio Website
+
+resource "aws_cloudfront_distribution" "website_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id   = "S3-Website"
+    
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.website_oai.cloudfront_access_identity_path
+    }
+  }
+  
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "CloudFront Distribution for Next.js Portfolio Website"
+  default_root_object = "index.html"
+  
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-Website"
+    
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+  
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+  
+  tags = {
+    Name = "Portfolio CloudFront"
+    Environment = "Production"
+  }
+}
---
+++ nextjs-blog/terraform-nextjs/output.tf
@@ -0,0 +1,17 @@
+output "bucket_website_endpoint" {
+  value = aws_s3_bucket.website.website_endpoint
+}
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.website_distribution.domain_name
+}
+
+
+
+
+
+
+# Output.tf is used to define the output values that will be displayed after the Terraform apply command is executed.
+# Output values are useful for displaying important information about the resources that have been created or modified by Terraform.
+# In this case, we are outputting the S3 bucket website endpoint and the CloudFront distribution domain name.
+# This information can be used to access the deployed Next.js portfolio website.
